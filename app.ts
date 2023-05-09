87d3289f2f3008b25d4623c3d13df2f00738d092
HarCass <harry.jc.cass@gmail.com>
2023-05-09 13:02:18 +0100

    Chat setup

--- app.ts
+++ app.ts
@@ -2,11 +2,34 @@ import express from "express";
 import cors from "cors";
 import apiRouter from "./routes/api";
 import { customErrors, serverErrors, dbErrors } from "./controllers/errors";
+import http from 'http';
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = new http.Server(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "exp://10.0.0.6:19000",
+    }
+});
 
 app.use(cors());
 app.use(express.json());
+
+interface chatUser {
+    username: string,
+    socketId: string
+}
+let users: string[] = [];
+io.on('connection', (socket) => {
+    console.log(`${socket.id} user connected!`);
+    if (!users.includes(socket.id)) users.push(socket.id);
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        users = users.filter((user) => user !== socket.id);
+        socket.disconnect();
+    });
+});
 
 app.use('/api', apiRouter);
 app.use(customErrors);
--- dist/app.js
+++ dist/app.js
@@ -7,9 +7,28 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const api_1 = __importDefault(require("./routes/api"));
 const errors_1 = require("./controllers/errors");
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
+const httpServer = new http_1.default.Server(app);
+const io = new socket_io_1.Server(httpServer, {
+    cors: {
+        origin: "exp://10.0.0.6:19000",
+    }
+});
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+let users = [];
+io.on('connection', (socket) => {
+    console.log(`${socket.id} user connected!`);
+    if (!users.includes(socket.id))
+        users.push(socket.id);
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        users = users.filter((user) => user !== socket.id);
+        socket.disconnect();
+    });
+});
 app.use('/api', api_1.default);
 app.use(errors_1.customErrors);
 app.use(errors_1.dbErrors);
